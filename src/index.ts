7294dc1cdbcd77fa7fd0f498ec54c91f82e4fd07
axetroy <troy450409405@gmail.com>
2019-04-03 15:25:56 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,23 @@ import { minify } from "./minify";
 
 export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("image.minify", async function(
-      uri: vscode.Uri,
-      fileList: vscode.Uri[]
-    ) {
-      const list = fileList && fileList.length ? fileList : [uri];
-      for (const file of list) {
-        try {
-          await minify(file);
-        } catch (err) {
-          console.error(err);
+    vscode.commands.registerCommand(
+      "image.minify",
+      async (uri: vscode.Uri, fileList: vscode.Uri[]) => {
+        const list = fileList && fileList.length ? fileList : [uri];
+        for (const file of list) {
+          try {
+            await minify(file);
+          } catch (err) {
+            console.error(err);
+          }
         }
       }
-    })
+    )
   );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate(context: vscode.ExtensionContext) {}
+export function deactivate(context: vscode.ExtensionContext) {
+  //
+}
